c2d117113f7f96fbe8b87756de65ff9b4192d3fb
Likith Sarapu <l.sarapu@zensar.com>
2023-08-17 19:05:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "../src/images/heart.jpg";
+import exampleImg from "../src/images/beer.jpg";
+// import imageUrl from '../src/images/heart.jpg';
+import whiteLoveIcon from "../src/images/white_heart.png";
+import redLoveIcon from "../src/images/red_heart.png";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 function App() {
+
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers?page=1&per_page=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setSmallImagesData(data);
+      });
+  }, []);
+
+  const [smallImagesData, setSmallImagesData] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top-text">
+        <div className="logo"></div>
+        <a>HOME</a>
+        <a>BEERS</a>
+        <a>CONTACT</a>
+      </div>
+      <div>
+        <img className="bgimage" src={exampleImg} />
+        <div className="image-text">
+          <h2>GREAT IPA</h2>
+          <p>
+            some hops is plowed. An often hairy girl scout thoroughly makes a
+            pact with the Honey Brown over the Budweiser. A blood clot makes a
+            pact with the discusting satelliste brewery. A lover about the
+            Pilsner Urquell takes a peek at the bottle of beer. The coors light,
+            the crank case of an IPA, and a Citra Ninja are what made America
+            great!
+          </p>
+        </div>
+      </div>
+      <div className="small-images">
+        {smallImagesData.length > 0 ? (
+          smallImagesData.map((imageData, index) => (
+            <Item imageData={imageData} index={index} setSmallImagesData={setSmallImagesData} />
+          ))
+        ) : (
+          <h1>Loading.....</h1>
+        )}
+      </div>
+
+      <div>
+        <div className="bottom-border"></div>
+        <div className="lastbottom-border">Copyright Foolproof 2019</div>
+      </div>
     </div>
   );
 }
 
+function Item({imageData, index, setSmallImagesData}){
+  const [isLiked, setIsLiked] = useState();
+  const handleLikeClick = (imageData, index) => {
+    setIsLiked((prev) => !prev);
+  };
+  return (
+    <div className="small-image" key={index}>
+              <div className="card-header">
+                {isLiked ? (
+                  <AiFillHeart
+                    className="love-icon"
+                    color="red"
+                    size={20}
+                    onClick={() => {handleLikeClick(imageData,index)}}
+                    />
+                    ) : (
+                      <AiOutlineHeart
+                      className="love-icon"
+                      size={20}
+                      onClick={() => {handleLikeClick(imageData,index)}}
+                  />
+                )}
+                <img src={imageData.image_url} alt={imageData.title} />
+              </div>
+              <div className="card-content">
+                <h2>{imageData.name}</h2>
+                <p>{imageData.description}</p>
+              </div>
+            </div>
+  );
+}
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,104 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders home, beers, and contact links", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLink = screen.getByText("HOME");
+  const beersLink = screen.getByText("BEERS");
+  const contactLink = screen.getByText("CONTACT");
+  expect(homeLink).toBeInTheDocument();
+  expect(beersLink).toBeInTheDocument();
+  expect(contactLink).toBeInTheDocument();
+});
+
+test("renders loading text when smallImagesData is empty", () => {
+  render(<App />);
+  const loadingText = screen.getByText("Loading.....");
+  expect(loadingText).toBeInTheDocument();
+});
+
+test("renders beer name and description when smallImagesData is available", async () => {
+  const mockData = [
+    {
+      name: "Test Beer",
+      description: "This is a test beer description.",
+      image_url: "test_image_url.jpg",
+    },
+  ];
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+
+  render(<App />);
+  const beerName = await screen.findByText("Test Beer");
+  const beerDescription = await screen.findByText("This is a test beer description.");
+  expect(beerName).toBeInTheDocument();
+  expect(beerDescription).toBeInTheDocument();
+});
+
+test("toggles heart icon when like button is clicked", async () => {
+  const mockData = [
+    {
+      name: "Test Beer",
+      description: "This is a test beer description.",
+      image_url: "test_image_url.jpg",
+    },
+  ];
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+
+  render(<App />);
+  const likeButton = await screen.findByTestId("like-button-0");
+  fireEvent.click(likeButton);
+  const filledHeartIcon = screen.getByTestId("filled-heart-icon-0");
+  expect(filledHeartIcon).toBeInTheDocument();
+});
+
+test("displays unfilled heart icon when like button is clicked twice", async () => {
+  const mockData = [
+    {
+      name: "Test Beer",
+      description: "This is a test beer description.",
+      image_url: "test_image_url.jpg",
+    },
+  ];
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+
+  render(<App />);
+  const likeButton = await screen.findByTestId("like-button-0");
+  fireEvent.click(likeButton);
+  fireEvent.click(likeButton);
+  const unfilledHeartIcon = screen.getByTestId("unfilled-heart-icon-0");
+  expect(unfilledHeartIcon).toBeInTheDocument();
+});
+
+test("displays beer name and description when like button is clicked", async () => {
+  const mockData = [
+    {
+      name: "Test Beer",
+      description: "This is a test beer description.",
+      image_url: "test_image_url.jpg",
+    },
+  ];
+  jest.spyOn(global, "fetch").mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+
+  render(<App />);
+  const likeButton = await screen.findByTestId("like-button-0");
+  fireEvent.click(likeButton);
+  const beerName = await screen.findByText("Test Beer");
+  const beerDescription = await screen.findByText("This is a test beer description.");
+  expect(beerName).toBeInTheDocument();
+  expect(beerDescription).toBeInTheDocument();
+});
+
+test("renders copyright text", () => {
+  render(<App />);
+  const copyrightText = screen.getByText("Copyright Foolproof 2019");
+  expect(copyrightText).toBeInTheDocument();
 });
